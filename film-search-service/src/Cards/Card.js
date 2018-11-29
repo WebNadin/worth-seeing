6d256b3f2fdf6bf4a48a4ce954a977c6cfac29f9
WebNadin <nenadinweb@gmail.com>
2018-11-29 14:18:17 +0200

    most popular films info were shown from imdb base

--- film-search-service/src/Cards/Card.js
+++ film-search-service/src/Cards/Card.js
@@ -2,75 +2,42 @@
  * Created by Nadin on 26.11.2018.
  */
 import React, { Component } from 'react';
-//import React from 'react';
 import axios from 'axios';
-import avatar from '../img/min/avatar.jpg';
+
+
 class Card extends Component {
-    constructor(films) {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             films: []
         }
     }
 
-    componentDidMount(films) {
-        const quantity = 3;
+    componentDidMount() {
+        const quantity = 2;
         axios.get(`http://localhost:3002/charts/most_popular_movies/rating/desc/${quantity}`)
             .then(res => {
-                for (var i = 0; i < res.data.length; i++) {
-                    const movies = res.data[i].name;
-                    var films = [];
-                     this.setState({films: movies})
-                    console.log('film' + i + ' = ');
-                    console.log(movies);
-                }
+                let filmsList = res.data.map((film) => {
+                    return (
+                        <div>
+                            <img src={film.poster} alt=""/>
+                            <p>{film.name}</p>
+                            <p>{film.rating}</p>
+                        </div>
+                    )
+                });
+                this.setState({films: filmsList});
+            })
 
-                console.log(films);
-
-            });
     }
 
     render() {
-        return (
-            <div className="card-group col-12 col-sm-4 mb-1 mb-sm-4 text-center">
-                <div className="card _pointer _no-shadow-hover _no-pointer-hover mb-0">
-                    <div className="card-body">
-
-                        <img src={avatar} alt="" className="card-img"/>
-                        <p className="card-text">
-                            test text
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                        <div className="nn_card__raiting"></div>
-                        <div className="nn_card__year"></div>
-                    </div>
-                </div>
-            </div>
-        );
+        const {films} = this.state;
+        return <div>{films}</div>
     }
+
 }
 
-
-/*const cardMin = (props) => {
- return (
- <div className="card-group col-12 col-sm-4 mb-1 mb-sm-4 text-center">
- <div className="card _pointer _no-shadow-hover _no-pointer-hover mb-0">
- <div className="card-body">
-
- <img class='' src='https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL50.jpg' alt="" className="card-img"/>
- <p className="card-text">
- {props.name}
- </p>
- </div>
- <div className="card-footer">
- <div className="nn_card__raiting"></div>
- <div className="nn_card__year"></div>
- </div>
- </div>
- </div>
- );
- };*/
-
 export default Card;
+
 
