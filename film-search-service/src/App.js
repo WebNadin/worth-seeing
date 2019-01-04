be6c470ef2b9299609e40e74e06e21187fe93478
WebNadin <nenadinweb@gmail.com>
2019-01-04 17:04:43 +0200

    sidebar component was created

--- film-search-service/src/App.js
+++ film-search-service/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
+import Rating from './components/Rating';
 //import Background from './img/bg-page.jpg';
 
 
@@ -15,6 +16,7 @@ class App extends Component {
                     {/*<div className="nn-load-more">
                      Загрузить еще 20 фильмов
                      </div>*/}
+                    <Rating />
                 </div>
             </div>
         );
