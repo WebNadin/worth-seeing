a680391834d27e55a1960a56e422ebd75b324079
WebNadin <nenadinweb@gmail.com>
2019-01-22 15:12:09 +0200

    chartType changes but rebuild of films date doesn't work

--- film-search-service/src/components/Card.js
+++ film-search-service/src/components/Card.js
@@ -14,79 +14,13 @@ export class Card extends Component {
         //this.chartType = "top_rated_tv";
         this.state = {
             films: []
-            //chartType: "top_rated_tv"
-            //chartType: this.props.chartType
         };
+        this.buildFilmsList = this.buildFilmsList.bind(this);
+        this.getFilmsData = this.getFilmsData.bind(this);
     }
 
-    /*loadFilmsData(nextProps, prevState) {
-     console.log('loadFilmsData () = ');
-     console.log('this.props.chartType loadFilmsData = ');
-     console.log(this.props.chartType);
-     console.log('this.chartType loadFilmsData = ');
-     console.log(this.chartType);
-     const quantity = 5;
-     if (nextProps.chartType) {
-     this.chartType = nextProps.chartType;
-     axios.get(`http://localhost:3002/charts/${this.chartType}/rating/desc/${quantity}`)
-     .then(res => {
-     let filmsList = res.data.map((film, index) => {
-     let imgUrl = film.poster;
-     let posterStyle = {
-     backgroundImage: 'url("' + imgUrl + '")'
-     };
-     let rating = Number(film.rating) * 10;
-     let styleStars = {
-     position: 'absolute',
-     top: 0,
-     left: 0,
-     overflow: 'hidden',
-     width: `${rating}%`
-     };
-     return (
-     <div key={index} className="nn-b_cards__item">
-     <div className="nn-card nn-card_dark bg-2">
-     <div className="nn-card__img-block" style={posterStyle}>
-     </div>
-     <div className="nn-card__title">{film.name}</div>
-     <div className="nn-card__rating nn-rating">
-     <div className="nn-rating__item _relative stars">
-     <img className='' src={starsGray} alt=""/>
-     <div className="stars_gray" style={styleStars}>
-     <img className='' src={stars} alt=""/>
-     </div>
-     </div>
-     <div className="nn-rating__item"><span>{film.rating}</span></div>
-     </div>
-
-     </div>
-     </div>
-     )
-     });
-     console.log('films before = ');
-     console.log(this.state.films);
-     this.setState({
-     films: filmsList
-     });
-     console.log('films after = ');
-     console.log(this.state.films);
-     });
-
-
-     }
-
-     }*/
-
-    componentDidMount() {
-        console.log('componentDidMount()');
-        console.log('loadFilmsData () = ');
-        console.log('this.props.chartType Did');
-        console.log(this.props.chartType);
-        console.log('this.chartType Did');
-        console.log(this.chartType);
+    getFilmsData() {
         const quantity = 5;
-
-        //this.chartType = this.state.chartType;
         axios.get(`http://localhost:3002/charts/${this.props.chartType}/rating/desc/${quantity}`)
             .then(res => {
                 let filmsList = res.data.map((film, index) => {
@@ -122,16 +56,66 @@ export class Card extends Component {
                         </div>
                     )
                 });
-                console.log('films before = ');
-                console.log(this.state.films);
                 this.setState({
                     films: filmsList
                 });
-                console.log('films after = ');
-                console.log(this.state.films);
             });
 
 
+    }
+
+    buildFilmsList(filmsList) {
+        console.log('buildFilmsList = ');
+        this.setState({
+            films: filmsList
+        });
+    }
+
+    componentDidMount() {
+        this.getFilmsData();
+        this.buildFilmsList();
+        //this.chartType = this.state.chartType;
+        /*axios.get(`http://localhost:3002/charts/${this.props.chartType}/rating/desc/${quantity}`)
+         .then(res => {
+         let filmsList = res.data.map((film, index) => {
+         let imgUrl = film.poster;
+         let posterStyle = {
+         backgroundImage: 'url("' + imgUrl + '")'
+         };
+         let rating = Number(film.rating) * 10;
+         let styleStars = {
+         position: 'absolute',
+         top: 0,
+         left: 0,
+         overflow: 'hidden',
+         width: `${rating}%`
+         };
+         return (
+         <div key={index} className="nn-b_cards__item">
+         <div className="nn-card nn-card_dark bg-2">
+         <div className="nn-card__img-block" style={posterStyle}>
+         </div>
+         <div className="nn-card__title">{film.name}</div>
+         <div className="nn-card__rating nn-rating">
+         <div className="nn-rating__item _relative stars">
+         <img className='' src={starsGray} alt=""/>
+         <div className="stars_gray" style={styleStars}>
+         <img className='' src={stars} alt=""/>
+         </div>
+         </div>
+         <div className="nn-rating__item"><span>{film.rating}</span></div>
+         </div>
+
+         </div>
+         </div>
+         )
+         });
+         this.setState({
+         films: filmsList
+         });
+         });
+
+         */
         //this.loadFilmsData(this.chartType);
     }
 
@@ -145,9 +129,8 @@ export class Card extends Component {
      return true;
      }*/
 
-    componentDidUpdate() {
-        console.log('componentDidUpdate()');
-        //this.loadFilmsData(this.chartType);
+    componentDidUpdate(filmsList) {
+
     }
 
     render() {
